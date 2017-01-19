d970387d88910a204c4fc1911d348ec55b28e226
Sasha15 <alex_brovko@mail.ru>
2017-01-19 10:25:07 +0200

    Header Styles and Animation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 ;(function ($) {
-
+	function menuAnimation(){
+		$('.menu-icon').click(function(){
+			if($(window).width()<641){
+				$('.sandwich-icon').toggleClass('open');
+				$('.top-bar-section').slideToggle();
+			}
+		})
+	}
 	$( document ).ready(function() {
+		$('.loupe').click(function(){
+			$('.search-form input, .search-form button').fadeToggle();
+		})
+		menuAnimation();
+
+	});
+	$(window).resize(function () {
 
 	});
 
