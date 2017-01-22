c7fe00d4c1a47c71fd2fe90c8723e15c4b35af23
Sasha15 <alex_brovko@mail.ru>
2017-01-22 18:12:36 +0200

    Added Slider and first section

--- js/main.js
+++ js/main.js
@@ -12,7 +12,13 @@
 			$('.search-form input, .search-form button').fadeToggle();
 		})
 		menuAnimation();
-
+		$('.main-slider').slick({
+			dots: false,
+			infinite: true,
+			speed: 300,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+		});
 	});
 	$(window).resize(function () {
 
